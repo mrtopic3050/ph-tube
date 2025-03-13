66ef1867782582022898a865dca2cacbe4e6fd27
mrtopic3050 <mrtopic50@gmail.com>
2025-03-14 05:42:59 +0600

    varified and not verified defined dynamically

--- script/index.js
+++ script/index.js
@@ -103,8 +103,8 @@ const displayVideos =(videos)=>{
 
                 </div>
                 <div class="intro">
-                    <h2 class="text-sm font-semibold">Shape of You</h2>
-                    <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt=""></p>
+                    <h2 class="text-sm font-semibold">${video.title}</h2>
+                    <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name} ${video.authors[0].verified == true?`<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="">` :``}</p>
                     <p class="text-sm text-gray-400 flex gap-">${video.others.views}</p>
 
                 </div>
